78682e873356bccbd872e846399a32b950977edf
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-14 14:09:16 -0500

    Created create_table function.

--- mutliplicationTable.py
+++ mutliplicationTable.py
@@ -3,8 +3,30 @@
 
 import sys
 import openpyxl
+from openpyxl.styles import Font
+from openpyxl.utils import get_column_letter
 
-wb = openpyxl.Workbook()
-sheet = wb.active
+N_value = int(input("Input value of N: "))
 
-wb.save("multiplication_table.xlsx")
+
+def create_table(N):
+    """Creates NxN table with bolded cell data."""
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    font_obj = Font(bold=True)
+
+    for row in range(1, N + 1):
+        sheet[f"A{row + 1}"] = row
+        sheet[f"A{row + 1}"].font = font_obj
+
+    for column in range(1, N + 1):
+        column_no = get_column_letter(column + 1)
+        sheet[f"{column_no}1"] = column
+        sheet[f"{column_no}1"].font = font_obj
+
+    wb.save("multiplication_table.xlsx")
+
+
+# TODO: Create loop to apply formula for cell calculations.
+
+create_table(N_value)
